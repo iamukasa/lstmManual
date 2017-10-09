2a880b5bb3936b374c276c12a4c90766e11f4a4f
iamukasa <amukach@gmail.com>
2017-10-09 13:51:36 -0400

    server py

--- plagarise/views.py
+++ plagarise/views.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('home')
+@app.route('/home')
 def startdis():
   return render_template("base.html")
 
--- runserver.py
+++ runserver.py
@@ -1,5 +1,5 @@
 from os import environ
-import app
+from plagarise import app
 
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
@@ -7,5 +7,4 @@ if __name__ == '__main__':
         PORT = int(environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
-app.run(HOST, PORT)
-app.run(HOST, PORT)
+app.run(HOST,PORT)
